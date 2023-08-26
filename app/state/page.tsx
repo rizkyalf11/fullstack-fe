1e44085125a80e216d0576ef61b1331342dc0708
IkkYa <akunikky11@gmail.com>
2023-08-26 11:09:20 +0700

    update

--- app/state/page.tsx
+++ app/state/page.tsx
@@ -1,128 +1,177 @@
-"use client"; // gunakan use client karena ada onChange pda komponen
-import { useState } from "react";
-import Button from "../component/Button";
+'use client'
+import React, { useState } from 'react'
+import Button from '../component/Button'
 
 type Identitas = {
-  nama: string;
-  sekolah: string;
-  umur: number | null;
-};
+	nama: string
+	sekolah: string
+	umur: number | null
+}
 
 type Hasil = {
-  mata_pelajaran: string;
-  nilai: number | null;
-};
+	mata_pelajaran: string
+	nilai: number | null
+}
 
-const State = () => {
-  let [message, setMessage] = useState("hai"); // jika string, dengan data awal "hai"
-  let [count, setCount] = useState(0); // jika number , dengan data awal 0
-  let [isLogin, setIsLogin] = useState(false); // jika booelan, dengan data awal false
-  let [profile, setProfile] = useState<Identitas>({
-    // jika sebuah object
-    nama: "Ilham Jaya kusuma",
-    sekolah: "SMK MADINATULQURAN",
-    umur: 17,
-  });
+const Home = () => {
+	const [message, setMessage] = useState('hello')
+	const [count, setCount] = useState(0)
+	const [isLogin, setIsLogin] = useState(false)
+	const [profile, setProfile] = useState<Identitas>({
+		nama: 'Ilham Jaya Kusuma',
+		sekolah: 'SMK Madinatul Quran',
+		umur: 17,
+	})
 
-  let [hasil, setHasil] = useState<Hasil[]>([
-    {
-      mata_pelajaran: "matematika",
-      nilai: 80,
-    },
-    {
-      mata_pelajaran: "fisika",
-      nilai: 90,
-    },
-    {
-      mata_pelajaran: "kimia",
-      nilai: 95,
-    },
-  ]); // jika sebuah array
+	let [hasil, setHasil] = useState<Hasil[]>([
+		{
+			mata_pelajaran: 'matematika',
+			nilai: 80,
+		},
+		{
+			mata_pelajaran: 'fisika',
+			nilai: 90,
+		},
+		{
+			mata_pelajaran: 'kimia',
+			nilai: 95,
+		},
+	])
 
-  return (
-    <main className="space-y-5">
-      <h1>Hello World</h1>
+	return (
+		<main>
+			<div>
+				<h2>Daftar Nilai</h2>
+				{hasil.map((n, index) => (
+					<section key={index}>
+						<h5>Nama Mata pelajaran : {n.mata_pelajaran} </h5>
+						<h5>Nilai : {n.nilai} </h5>
+					</section>
+				))}
+        <Button
+          title="Tambah"
+          variant="solid"
+          colorSchema="blue"
+          onClick={() => {
+            setHasil((prevHasil) => {
+              return [
+                ...prevHasil,
+                {
+                  mata_pelajaran: "B. Indonesia",
+                  nilai: 100,
+                },
+              ];
+            });
+          }}
+        />
+        <Button
+          title="Kurang"
+          isDisabled={hasil.length <= 1}
+          variant="solid"
+          colorSchema="red"
+          onClick={() => {
+            setHasil((prevHasil) => {
+              prevHasil.pop();
+              return [...prevHasil];
+            });
+          }}
+        />
+			</div>
 
-      <p>message addalah {message}</p>
+			<h1 className="text-2xl m-2">
+				nama adalah {profile.nama} dan sekolah di {profile.sekolah} alamat -
+			</h1>
+			<Button
+				title="ubah nama"
+				colorSchema="blue"
+				variant="solid"
+				onClick={() => {
+					setProfile((prevProfile) => {
+						return {
+							...prevProfile,
+							nama: 'rizky a',
+						}
+					})
+				}}
+			/>
+			<Button
+				title="reset"
+				colorSchema="blue"
+				variant="solid"
+				onClick={() => {
+					setProfile((prevProfile) => {
+						return {
+							...prevProfile,
+							nama: 'Ilham Jaya Kusuma',
+						}
+					})
+				}}
+			/>
 
-      <p>Count adalah {count}</p>
+			<h1 className="text-2xl">{message}</h1>
+			<Button
+				title="button 1"
+				colorSchema="blue"
+				variant="solid"
+				onClick={() => {
+					setMessage('hello')
+				}}
+			/>
+			<Button
+				title="button 2"
+				colorSchema="red"
+				variant="solid"
+				onClick={() => {
+					setMessage('hello ihsan')
+				}}
+			/>
 
-      <p> {isLogin ? "Sudah Login" : "Belum Login"}</p>
+			<h1 className="text-2xl">{count}</h1>
+			<Button
+				title="button +"
+				colorSchema="blue"
+				variant="solid"
+				onClick={() => {
+					setCount((prev) => {
+						console.log(prev)
+						return prev + 1
+					})
+				}}
+			/>
+			<Button
+				title="button -"
+				colorSchema="blue"
+				variant="solid"
+				isDisabled={count === 0}
+				onClick={() => {
+					setCount((prev) => {
+						console.log(prev)
+						return prev - 1
+					})
+				}}
+			/>
 
-      <div className="flex flex-col">
-        <h5>Nama : {profile.nama}</h5>
-        <h5>Sekolah : {profile.sekolah}</h5>
-        <h5>Umur : {profile.umur}</h5>
-      </div>
+			<h1>{isLogin ? 'Sudah Login' : 'Belum Login'}</h1>
+			<Button
+				title="log in"
+				colorSchema="blue"
+				variant="solid"
+				isDisabled={isLogin}
+				onClick={() => {
+					setIsLogin(true)
+				}}
+			/>
+			<Button
+				title="log out"
+				colorSchema="blue"
+				variant="solid"
+				isDisabled={!isLogin}
+				onClick={() => {
+					setIsLogin(false)
+				}}
+			/>
+		</main>
+	)
+}
 
-      <div>
-        <h2>Daftar Nilai</h2>
-        {hasil.map((n, index) => (
-          <section key={index}>
-            <h5>Nama Mata pelajaran : {n.mata_pelajaran} </h5>
-            <h5>Nilai : {n.nilai} </h5>
-          </section>
-        ))}
-      </div>
-
-      <Button
-        title="Hello"
-        variant="solid"
-        colorSchema="blue"
-        onClick={() => {
-          setMessage("Hello");
-        }}
-      />
-
-      <Button
-        title="Salam"
-        variant="outline"
-        colorSchema="blue"
-        onClick={() => {
-          setMessage("Assalamualaikum");
-        }}
-      />
-
-    <p>Count adalah {count}</p>
-      <Button
-        title="Tambah"
-        variant="solid"
-        colorSchema="blue"
-        onClick={() => {
-          setCount((prevCount) => prevCount + 1);
-        }}
-      />
-      <Button
-        title="Kurang"
-        isDisabled={count < 1}
-        variant="solid"
-        colorSchema="red"
-        onClick={() => {
-          setCount((prevCount) => prevCount - 1);
-        }}
-      />
-
-    <p> {isLogin ? "Sudah Login" : "Belum Login"}</p>
-      <Button
-        title="Login"
-        isDisabled={isLogin === true}
-        variant="solid"
-        colorSchema="blue"
-        onClick={() => {
-          setIsLogin(true);
-        }}
-      />
-      <Button
-        title="Logout"
-        isDisabled={isLogin === false}
-        variant="solid"
-        colorSchema="red"
-        onClick={() => {
-          setIsLogin(false);
-        }}
-      />
-    </main>
-  );
-};
-
-export default State;
+export default Home
